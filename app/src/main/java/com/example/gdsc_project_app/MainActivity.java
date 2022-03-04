dcca55d0031383a121915449ab94e29dd246bd9a
VedWalavalkar <vtw5023@psu.edu>
2022-03-03 20:15:14 -0500

    Ved's commit

--- app/src/main/java/com/example/gdsc_project_app/MainActivity.java
+++ app/src/main/java/com/example/gdsc_project_app/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     // this is a comment from Brian this is from main
     // this is a comment from Nick
     // second comment from Nick
+    // this is a comment from Ved
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
