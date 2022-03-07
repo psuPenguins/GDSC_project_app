631e439e8dcca8ad63d67707d098df12cb9c29ce
VedWalavalkar <vtw5023@psu.edu>
2022-03-07 17:10:48 -0500

    Linked Room and Swipe Home Buttons

--- app/src/main/java/com/example/gdsc_project_app/MainActivity.java
+++ app/src/main/java/com/example/gdsc_project_app/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.gdsc_project_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
     //TODO: create private variables (btn, tv..)
+    private Button btnLogOut;
+    private Button btnSwipe;
+    private Button btnRoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,39 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "I'm in MainActivity");
 
         // TODO: link the private variables to the elements in the xml files
+        btnLogOut = findViewById((R.id.btnLogOut));
+        btnSwipe = findViewById((R.id.btnSwipe));
+        btnRoom = findViewById((R.id.btnRoom));
+
+        // TODO: create onclicklistener for the button to the change the activity
+        btnSwipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Log.i(TAG, "onClick Swipe button");
+                goSwipeActivity();
+            }
+        });
+
+        btnRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Log.i(TAG, "onClick Room button");
+                goRoomActivity();
+            }
+        });
     }
 
-    // TODO: create onclicklistener for the button to the change the activity
+
+
+    //declaring transition functions
+    private void goSwipeActivity(){
+        Intent i = new Intent(this, SwipeActivity.class);
+        Log.i(TAG, "Going into SwipeActivity");
+        startActivity(i);
+    }
+    private void goRoomActivity(){
+        Intent i = new Intent(this, RoomActivity.class);
+        Log.i(TAG, "Going into RoomActivity");
+        startActivity(i);
+    }
 }
