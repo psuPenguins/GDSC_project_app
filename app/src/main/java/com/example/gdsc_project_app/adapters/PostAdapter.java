018eb9d67e7d688a817ffe4d69e9a21ca90670ee
Selina0w0 <skf5373@psu.edu>
2022-03-07 13:35:57 -0500

    commit from Selina: stopped building room adapters at video 16:30

--- app/src/main/java/com/example/gdsc_project_app/adapters/PostAdapter.java
+++ app/src/main/java/com/example/gdsc_project_app/adapters/PostAdapter.java
@@ -1,5 +1,6 @@
 package com.example.gdsc_project_app.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,17 +13,32 @@ import com.example.gdsc_project_app.R;
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
 
+    Context context;
+    List<Post> posts;
+
+    public PostAdapter(Context context, List<Post> posts) {
+        this.context = context;
+        this.posts = posts;
+    }
+
+    // Inflating a layout from XML and returning the holder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View postView = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(postView);
     }
 
+    // Populating data into the item through holder
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        // Get the post at the passed in position
+        Post post = posts.get(position);
+        // Bind the post data into the ViewHolder
+        holder.bind(post);
     }
 
+    // Returns the total count of items in the list
     @Override
     public int getItemCount() {
         return 0;
@@ -40,4 +56,6 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
             imUserPic = itemView.findViewById(R.id.ivUserPic);
         }
     }
+
+    // STOPPED AT 16:30
 }
