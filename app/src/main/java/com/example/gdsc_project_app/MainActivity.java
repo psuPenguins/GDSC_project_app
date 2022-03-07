01640a9bb8f05709570d7f8b78caa782b3d453d5
Chen-Kuan Liao <czl5899@psu.edu>
2022-03-07 10:47:23 -0500

    adding log in mainActivity

--- app/src/main/java/com/example/gdsc_project_app/MainActivity.java
+++ app/src/main/java/com/example/gdsc_project_app/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.gdsc_project_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
     // this is a comment from Brian this is from main second time
     // this is a comment from Nick
     // second comment from Nick
@@ -14,5 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "I'm in MainActivity");
     }
 }
