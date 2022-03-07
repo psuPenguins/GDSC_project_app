1bf338cabda5008743bf6a8c654634029f972280
Chen-Kuan Liao <czl5899@psu.edu>
2022-03-07 16:34:24 -0500

    added line for RoomActivity to set the var

--- app/src/main/java/com/example/gdsc_project_app/RoomActivity.java
+++ app/src/main/java/com/example/gdsc_project_app/RoomActivity.java
@@ -1,6 +1,7 @@
 package com.example.gdsc_project_app;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +10,7 @@ public class RoomActivity extends AppCompatActivity {
     public static final String TAG = "RoomActivity";
 
     //TODO: create private variables (btn, tv..)
+    private Button buttonViewReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class RoomActivity extends AppCompatActivity {
         setContentView((R.layout.activity_room));
 
         // TODO: link the private variables to the elements in the xml files
+        buttonViewReply = findViewById((R.id.buttonViewReply));
     }
 
     // TODO: create onclicklistener for the button to the change the activity
