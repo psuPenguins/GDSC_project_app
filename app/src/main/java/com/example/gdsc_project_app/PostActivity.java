5419d0f04568c8d81af79cbdd3c1c10c239b0549
Selina0w0 <skf5373@psu.edu>
2022-03-08 13:25:20 -0500

    commit from Selina: linking acticity post

--- app/src/main/java/com/example/gdsc_project_app/PostActivity.java
+++ app/src/main/java/com/example/gdsc_project_app/PostActivity.java
@@ -1,6 +1,43 @@
 package com.example.gdsc_project_app;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class PostActivity extends AppCompatActivity {
+    public static final String TAG = "PostActivity";
+
+    // create private variables (btn, tv..)
+    private Button btnMakePost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((R.layout.activity_post));
+        Log.i(TAG, "I'm in PostActivity");
+
+        // link the private variables to the elements in the xml files
+        btnMakePost=findViewById(R.id.btnMakePost);
+
+        btnMakePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Log.i(TAG, "onClick make post button");
+                goRoomActivity();
+            }
+        });
+    }
+
+    // declaring transition functions
+    private void goRoomActivity(){
+        Intent i = new Intent(this, RoomActivity.class);
+        Log.i(TAG, "Going into RoomActivity");
+        startActivity(i);
+    }
 }
