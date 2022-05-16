eb7525d1d7c2f54a606e1c6ac5d92dc8fcb4215c
Selina0w0 <skf5373@psu.edu>
2022-05-16 18:54:58 +0800

    commit from Selina: set profile pic and username in ProfileFragment

--- app/src/main/java/com/example/gdsc_project_app/ProfileFragment.java
+++ app/src/main/java/com/example/gdsc_project_app/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.gdsc_project_app;
 
+import static com.example.gdsc_project_app.User.KEY_USER_PROFILE_IMAGE;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +10,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.core.view.accessibility.AccessibilityViewCommand;
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProfileFragment extends Fragment {
     public static final String TAG = "ProfileFragment";
     public Button btnLogOut;
     public ImageView ivProfilePic;
+    public TextView tvProfileUsername;
 
     public ProfileFragment(){ }
 
@@ -31,7 +45,7 @@ public class ProfileFragment extends Fragment {
 
         btnLogOut = view.findViewById(R.id.btnLogOut);
         ivProfilePic = view.findViewById(R.id.ivProfilePic);
-
+        tvProfileUsername = view.findViewById(R.id.tvProfileUsername);
 
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +54,11 @@ public class ProfileFragment extends Fragment {
                 goLoginActivity();
             }
         });
+
+        // Setting the shown user pic and username
+        Glide.with(getActivity()).load(ParseUser.getCurrentUser().getParseFile(KEY_USER_PROFILE_IMAGE).getUrl()).into(ivProfilePic);
+        tvProfileUsername.setText(ParseUser.getCurrentUser().getUsername());
+
     }
 
     private void goLoginActivity(){
