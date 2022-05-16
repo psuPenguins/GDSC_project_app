f1f089eb00af5d80162c26347c6e85a6637e3a1a
Selina0w0 <skf5373@psu.edu>
2022-05-16 18:24:43 +0800

    commit from Selina: add fragment icon pics

--- app/src/main/java/com/example/gdsc_project_app/MainActivity.java
+++ app/src/main/java/com/example/gdsc_project_app/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -15,7 +16,7 @@ import androidx.fragment.app.FragmentTransaction;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-    Button swipeFragmentBtn, roomFragmentBtn, profileFragmentBtn;
+    ImageButton swipeFragmentBtn, roomFragmentBtn, profileFragmentBtn;
 
 
     @Override
