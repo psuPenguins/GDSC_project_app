9e72bb0915f2f88a1c1f7f494151d77b98520fb2
Selina0w0 <skf5373@psu.edu>
2022-05-23 16:51:46 +0800

    commit from Selina: updated name of database variables

--- app/src/main/java/com/example/gdsc_project_app/FBPost.java
+++ app/src/main/java/com/example/gdsc_project_app/FBPost.java
@@ -1,13 +1,13 @@
 package com.example.gdsc_project_app;
 
 public class FBPost {
-    public String userID, username, topicID, description;
+    public String userID, username, roomID, description;
     public Integer likeCount, dislikeCount;
 
-    public FBPost(String userID, String username, String topicID, String description) {
+    public FBPost(String userID, String username, String roomID, String description) {
         this.userID = userID;
         this.username = username;
-        this.topicID = topicID;
+        this.roomID = roomID;
         this.description = description;
         this.likeCount = 0;
         this.dislikeCount = 0;
