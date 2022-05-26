692dd39d4bb59406cc9a70ec2396723b92faa482
Selina0w0 <skf5373@psu.edu>
2022-05-26 23:32:18 +0800

    commit from Selina: got Brian't work back

--- app/src/main/java/com/example/gdsc_project_app/FBTopic.java
+++ app/src/main/java/com/example/gdsc_project_app/FBTopic.java
@@ -2,6 +2,9 @@ package com.example.gdsc_project_app;
 
 public class FBTopic {
     public String question, topic;
+
+    public FBTopic(){};
+
     public FBTopic(String question, String topic) {
         this.question = question;
         this.topic = topic;
--- app/src/main/java/com/example/gdsc_project_app/SwipeFragment.java
+++ app/src/main/java/com/example/gdsc_project_app/SwipeFragment.java
@@ -17,11 +17,21 @@ import android.widget.PopupWindow;
 
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import com.mindorks.placeholderview.SwipePlaceHolderView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SwipeFragment extends Fragment {
@@ -29,6 +39,15 @@ public class SwipeFragment extends Fragment {
     public static final String TAG = "SwipeFragment";
     private SwipePlaceHolderView swipeView;
     //private Context someContext;
+
+    //FBUser user;
+    FirebaseUser currentUser;
+    String currentUserUID;
+    String currentUsername;
+    ArrayList<String> questions = new ArrayList<String>();
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference topicsRef = database.getReference("Topics");
 
     public SwipeFragment() { }
 
@@ -56,20 +75,47 @@ public class SwipeFragment extends Fragment {
 
     // generate cards from data base
     private void queryQuestion() {
-        ParseQuery<Question> query = new ParseQuery<>("Question");
-        query.findInBackground(new FindCallback<Question>() {
+//        ParseQuery<Question> query = new ParseQuery<>("Question");
+//        query.findInBackground(new FindCallback<Question>() {
+//            @Override
+//            public void done(List<Question> dbQs, ParseException e) {
+//                Point p = new Point(0, 0); // Point maybe useless, don't know yet.
+//                if (e == null) {
+//                    for (Question q : dbQs) {
+//                        Log.i(TAG, q.getQuestion());
+//                        Log.i(TAG, "" + swipeView.getChildCount());
+//                        swipeView.addView(new SwipeItem(getActivity(), swipeView, getApplicationContext(), q.getQuestion(), p));
+//                    }
+//                } else {
+//                    Log.e(TAG, "ERROR getting data");
+//                }
+//            }
+//        });
+        //Getting User
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        currentUserUID = currentUser.getUid();
+        Log.i(TAG, "PF CurrentUID:"+currentUserUID);
+        //Getting Username
+        topicsRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void done(List<Question> dbQs, ParseException e) {
-                Point p = new Point(0, 0); // Point maybe useless, don't know yet.
-                if (e == null) {
-                    for (Question q : dbQs) {
-                        Log.i(TAG, q.getQuestion());
-                        Log.i(TAG, "" + swipeView.getChildCount());
-                        swipeView.addView(new SwipeItem(getActivity(), swipeView, getApplicationContext(), q.getQuestion(), p));
-                    }
-                } else {
-                    Log.e(TAG, "ERROR getting data");
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                questions.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    FBTopic topic = snapshot.getValue(FBTopic.class);
+                    questions.add(topic.question);
+                    Log.i(TAG, "topic question: " + topic.question);
                 }
+
+                for (String question : questions) {
+                    Point p = new Point(0, 0); // Point maybe useless, don't know yet.
+                    Log.i(TAG, "" + swipeView.getChildCount());
+                    swipeView.addView(new SwipeItem(getActivity(), swipeView, getApplicationContext(), question, p));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.i(TAG, "The read failed for user: " + databaseError.getCode());
             }
         });
     }
